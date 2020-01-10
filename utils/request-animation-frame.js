bf40eb044c170032c2754ccc028ad3d870c6dde6
Andy <andreydmitrievich666@gmail.com>
2020-01-10 16:12:13 +0300

    update requestAnimationFrameComponent

--- utils/request-animation-frame.js
+++ utils/request-animation-frame.js
@@ -4,24 +4,34 @@
  * Example:
  * requestAnimationFrameComponent.add(functionName);
  *
- * @method {{ add}}
+ * @method {{ add }}
  */
 const requestAnimationFrameComponent = (() => {
     const self = {
         callbacks: [],
+        time: 0
     };
 
     /**
      * добавляем новый колбэк в очередь
-     * @param {{callback: function}} callback
+     * @param {{callback: function, context}} callback
      */
-    const add = (callback) => {
-        self.callbacks.push(callback);
+    const add = (callback, ctx) => {
+        self.callbacks.push({
+            callback: callback,
+            ctx: ctx
+        });
     };
 
     function init() {
+        let deltaTime = timestamp - self.time;
+        self.time = timestamp;
         self.callbacks.forEach((element) => {
-            element();
+            if (!element.ctx) {
+                element.callback(deltaTime);
+            } else {
+                element.callback.apply(element.ctx, [deltaTime]);
+            }
         });
         requestAnimationFrame(init);
     }
