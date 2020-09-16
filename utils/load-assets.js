0d0b08fd86fca827744bfa723a4f8797a9bf18a0
Andy <andreydmitrievich666@gmail.com>
2020-09-16 16:54:57 +0300

    update loadScript

--- utils/load-assets.js
+++ utils/load-assets.js
@@ -3,25 +3,38 @@
  * @param {string} src
  * @returns {Promise} Promise(script object)
  */
-const loadScript = (src) => {
+onst loadScript = (src) => {
     const scripts = [...document.scripts].filter(script => script.src === src);
     return new Promise((resolve, reject) => {
         if (scripts.length) {
-            if (document.readyState === 'complete') {
-                resolve(scripts);
-            }
-            document.addEventListener('DOMContentLoaded', () => {
-                resolve(scripts);
-            });
-        } else {
-            const script = document.createElement('script');
-            script.src = src;
-            script.onerror = () => {
-                reject(script);
-            };
-            script.onload = () => {
+            let script = scripts[scripts.length - 1];
+
+            if(script.dataset.load === 'loaded') { // Проверяем загружен ли скрипт, если нет, то ждем загрузки
                 resolve(script);
-            };
+            } else if(script.dataset.load === 'error') {
+                reject(script);
+            } else {
+                script.addEventListener('error', ()=>{
+                    script.dataset.load = 'error';
+                    reject(script);
+                });
+                script.addEventListener('load', ()=>{
+                    script.dataset.load = 'loaded';
+                    resolve(script);
+                });
+            }
+
+        } else {
+            let script = document.createElement('script');
+            script.src = src;
+            script.addEventListener('error', ()=>{
+                script.dataset.load = 'error';
+                reject(script);
+            });
+            script.addEventListener('load', ()=>{
+                script.dataset.load = 'loaded'; // Помечаем, что скрипт уже загружен
+                resolve(script);
+            });
             document.body.appendChild(script);
         }
     });
